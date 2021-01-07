061d213f690b3c4d16450549e98389457a2e805b
Javier Zarate <javalzar17@gmail.com>
2021-01-06 17:56:54 -0800

    x is now added to object that keeps tracks of moves. X cant override existing move.

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,79 +1,125 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
 ctx.lineWidth = 2;
-ctx.beginPath();
 
-// mouse coordinates
-let mouseX, mouseY;
+let board = {
+  s1: "", s2: "", s3: "",
+  s4: "", s5: "", s6: "",
+  s7: "", s8: "", s9: ""
+}
 
 // listens for click and drax x at given coordinates
 canvas.addEventListener("mouseup", mouseUp, false);
 
 function drawX(x, y) {
-    ctx.moveTo(x - 20, y - 20);
-    ctx.lineTo(x + 20, y + 20);
-    ctx.stroke();
+  ctx.beginPath();
+  // line 1
+  ctx.moveTo(x - 35, y - 35);
+  ctx.lineTo(x + 35, y + 35);
+  ctx.stroke();
 
-    ctx.moveTo(x + 20, y - 20);
-    ctx.lineTo(x - 20, y + 20);
-    ctx.stroke();
+  // line 2 (striking over line 1)
+  ctx.moveTo(x + 35, y - 35);
+  ctx.lineTo(x - 35, y + 35);
+  ctx.stroke();
 }
 
 function mouseUp(e) {
-    mouseX = e.pageX - canvas.offsetLeft;
-    mouseY = e.pageY - canvas.offsetTop;
-    console.log(mouseX, mouseY)
+  // mouse coordinates
+  let mouseX, mouseY;
+  // offsetting coordinates on page
+  // to proper coordinates within canvas
+  mouseX = e.pageX - canvas.offsetLeft;
+  mouseY = e.pageY - canvas.offsetTop;
+  console.log(mouseX, mouseY)
 
-    // first column
-    if (mouseX > 0 && mouseX < 99) {
-      // row1
-      if (mouseY > 0 && mouseY < 99) {
+  // first column
+  if (mouseX > 0 && mouseX < 99) {
+    // row1 (s1)
+    if (mouseY > 0 && mouseY < 99) {
+      if (board.s1 === "") {
+        board.s1 = "x";
         drawX(50, 50)
       }
-      // row 2
-      if (mouseY > 101 && mouseY < 199) {
+      console.log(board)
+    }
+    // row 2 (s4)
+    if (mouseY > 101 && mouseY < 199) {
+      if (board.s4 === "") {
+        board.s4 = "x";
         drawX(50, 150)
       }
-      // row 3
-      if (mouseY > 201 && mouseY < 299) {
+      console.log(board)
+    }
+    // row 3 (s7)
+    if (mouseY > 201 && mouseY < 299) {
+      if (board.s7 === "") {
+        board.s7 = "x";
         drawX(50, 250)
       }
+      console.log(board)
     }
-    // second column
-    if (mouseX > 101 && mouseX < 199) {
-      // row1
-      if (mouseY > 0 && mouseY < 99) {
+  }
+  // second column
+  if (mouseX > 101 && mouseX < 199) {
+    // row1 (s2)
+    if (mouseY > 0 && mouseY < 99) {
+      if (board.s2 === "") {
+        board.s2 = "x";
         drawX(150, 50)
       }
-      // row 2
-      if (mouseY > 101 && mouseY < 199) {
+      console.log(board)
+    }
+    // row 2 (s5)
+    if (mouseY > 101 && mouseY < 199) {
+      if (board.s5 === "") {
+        board.s5 = "x";
         drawX(150, 150)
       }
-      // row 3
-      if (mouseY > 201 && mouseY < 299) {
+      console.log(board)
+    }
+    // row 3 (s8)
+    if (mouseY > 201 && mouseY < 299) {
+      if (board.s8 === "") {
+        board.s8 = "x";
         drawX(150, 250)
       }
+      console.log(board)
     }
-    // third column
-    if (mouseX > 201 && mouseX < 299) {
-      // row1
-      if (mouseY > 0 && mouseY < 99) {
+  }
+  // third column
+  if (mouseX > 201 && mouseX < 299) {
+    // row1 (s3)
+    if (mouseY > 0 && mouseY < 99) {
+      if (board.s3 === "") {
+        board.s3 = "x";
         drawX(250, 50)
       }
-      // row 2
-      if (mouseY > 101 && mouseY < 199) {
+      console.log(board)
+    }
+    // row 2 (s6)
+    if (mouseY > 101 && mouseY < 199) {
+      if (board.s6 === "") {
+        board.s6 = "x";
         drawX(250, 150)
       }
-      // row 3
-      if (mouseY > 201 && mouseY < 299) {
+      console.log(board)
+    }
+    // row 3 (s9)
+    if (mouseY > 201 && mouseY < 299) {
+      if (board.s9 === "") {
+        board.s9 = "x";
         drawX(250, 250)
       }
+      console.log(board)
     }
+  }
 
 }
 
 
 function renderBoard() {
+  ctx.beginPath();
   ctx.moveTo(100, 0);
   ctx.lineTo(100, 300);
   ctx.moveTo(200, 0);
