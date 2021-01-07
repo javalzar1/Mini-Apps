eb792e336856f66a5a7a265682587d6d102466b0
Javier Zarate <javalzar17@gmail.com>
2021-01-06 17:24:25 -0800

    Anywhere within proper field X is rendered in the center.

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -22,9 +22,54 @@ function drawX(x, y) {
 function mouseUp(e) {
     mouseX = e.pageX - canvas.offsetLeft;
     mouseY = e.pageY - canvas.offsetTop;
-    console.log(e.pageX, e.pageY)
+    console.log(mouseX, mouseY)
 
-    drawX(mouseX, mouseY);
+    // first column
+    if (mouseX > 0 && mouseX < 99) {
+      // row1
+      if (mouseY > 0 && mouseY < 99) {
+        drawX(50, 50)
+      }
+      // row 2
+      if (mouseY > 101 && mouseY < 199) {
+        drawX(50, 150)
+      }
+      // row 3
+      if (mouseY > 201 && mouseY < 299) {
+        drawX(50, 250)
+      }
+    }
+    // second column
+    if (mouseX > 101 && mouseX < 199) {
+      // row1
+      if (mouseY > 0 && mouseY < 99) {
+        drawX(150, 50)
+      }
+      // row 2
+      if (mouseY > 101 && mouseY < 199) {
+        drawX(150, 150)
+      }
+      // row 3
+      if (mouseY > 201 && mouseY < 299) {
+        drawX(150, 250)
+      }
+    }
+    // third column
+    if (mouseX > 201 && mouseX < 299) {
+      // row1
+      if (mouseY > 0 && mouseY < 99) {
+        drawX(250, 50)
+      }
+      // row 2
+      if (mouseY > 101 && mouseY < 199) {
+        drawX(250, 150)
+      }
+      // row 3
+      if (mouseY > 201 && mouseY < 299) {
+        drawX(250, 250)
+      }
+    }
+
 }
 
 
