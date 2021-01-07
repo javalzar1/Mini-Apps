e653501a1a0e6b0b2eae69a878cac020387cc25c
Javier Zarate <javalzar17@gmail.com>
2021-01-06 16:55:04 -0800

    only using canvas and mouseup event listerner to draw x, forming logic to draw x only within proper area.

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,11 +1,34 @@
-function test() {
-  alert('tst')
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+ctx.lineWidth = 2;
+ctx.beginPath();
+
+// mouse coordinates
+let mouseX, mouseY;
+
+// listens for click and drax x at given coordinates
+canvas.addEventListener("mouseup", mouseUp, false);
+
+function drawX(x, y) {
+    ctx.moveTo(x - 20, y - 20);
+    ctx.lineTo(x + 20, y + 20);
+    ctx.stroke();
+
+    ctx.moveTo(x + 20, y - 20);
+    ctx.lineTo(x - 20, y + 20);
+    ctx.stroke();
 }
 
-renderBoard = () => {
-  var c = document.getElementById("canvas");
-  var ctx = c.getContext("2d");
-  ctx.beginPath();
+function mouseUp(e) {
+    mouseX = e.pageX - canvas.offsetLeft;
+    mouseY = e.pageY - canvas.offsetTop;
+    console.log(e.pageX, e.pageY)
+
+    drawX(mouseX, mouseY);
+}
+
+
+function renderBoard() {
   ctx.moveTo(100, 0);
   ctx.lineTo(100, 300);
   ctx.moveTo(200, 0);
@@ -14,6 +37,5 @@ renderBoard = () => {
   ctx.lineTo(300, 100);
   ctx.moveTo(0, 200);
   ctx.lineTo(300, 200);
-
   ctx.stroke();
 }
